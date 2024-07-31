f09b74e73f40486c38f754dd41cbdc03dae9450b
degorov <egorovdvrsreu@gmail.com>
2024-07-31 20:55:29 +0300

    task3 done

--- task3/task3.py
+++ task3/task3.py
@@ -1,26 +1,32 @@
 #функция возвращает информацию о файле - словарь вида:
-#{'name':<имя файла>, 'lines_amount':<к-во строк в файле>, 
-# 'first_line':<1-ая строка>}
+#{'name':<имя файла>, 'lines_amount':<к-во строк в файле>
 def file_info(name, file):
-    #читаем первую строку
-    first_line = file.readline().strip()
-    lines_amount = 1
-    #считаем оставшееся количество строк
+    lines_amount = 0
+    #считаем количество строк
     for line in file:
         lines_amount += 1
-    return {'name':name, 'lines_amount':lines_amount, 'first_line':first_line}
+    return {'name':name, 'lines_amount':lines_amount}
 
 
-
+#список файлов для задачи (лежат в той же папке, что и программа)
 files = ['1.txt', '2.txt', '3.txt', 'empty_lines.txt']
-files_info = []         #---------------------а нужна ли глобальная переменная-------------------------
+#итоговый файл для объединения вышеупомянутых
+result_file = "result.txt"
+#узнаём количество строк данных файлов, записываем в список из словарей
+files_info = []
 for file_name in files:
     with open(file_name) as file:
         files_info.append(file_info(file_name, file))
-#сортируем информацию о файлах по количеству строк в конкретном файле
+#сортируем список по количеству строк в конкретном файле
 files_info = sorted(files_info, key = lambda file_dict: 
                     file_dict['lines_amount'])
-print(files_info)
 
-# files_info_sorted = sorted(files_info, key = lambda file_dict: file_dict['lines_amount'])
-# print(files_info_sorted)
+#создаём новый итоговый файл (если уже существует, то полностью перезапишется);
+#записываем в него по очереди, в порядке количества строк:
+#имя одного из файлов, кол-во строк, его содержимое
+with open(result_file, mode = 'w') as file_1:
+    for file_2_dict in files_info:
+        file_1.write(file_2_dict['name'] + '\n')
+        file_1.write(str(file_2_dict['lines_amount']) + '\n')
+        with open(file_2_dict['name']) as file_2:
+            file_1.write(file_2.read() + '\n')
